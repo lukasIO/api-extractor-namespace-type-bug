5ce0d22b65642e80e32ddd80f34a65140190fac7
lukasIO <mail@lukasseiler.de>
2024-10-09 17:55:54 +0200

    first commit

---
+++ bar/index.ts
@@ -0,0 +1,7 @@
+import { utils } from "foo";
+
+export const someFunction = () => {
+  utils.sanityCheck();
+};
+
+export const aTypedObject: utils.FooType = { flag: false };
---
+++ foo/utils/debug.ts
@@ -0,0 +1,7 @@
+export type FooType = {
+  flag: boolean;
+};
+
+export const sanityCheck = () => {
+  console.log("still sane");
+};
